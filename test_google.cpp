149f9089fc684c7f611d834157cdedb5eb022231
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-05-03 17:07:40 +0300

    Update test_google.cpp

--- test_google.cpp
+++ test_google.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
-//#include "lib.h"
-
-//extern "C"
-//{
 #include "lib.h"
-//}
+
 
 TEST(test_version, test_valid_version)
 {
     EXPECT_EQ(0, version() > 0);
 }
 
-//int main(int argc, char** argv) {
-//    testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-//}
+int main(int argc, char** argv) {
+   testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
